f711f49cd974f5ebe09ef82d9ab8910ac50fae26
Marcus Freemon <dfreemon@gmail.com>
2014-05-13 10:48:09 -0700

    semantic changes

---
+++ js/email_test.js
@@ -0,0 +1,19 @@
+
+<script type = "text/javascript">
+
+
+
+function validateEmail()
+{
+ var email = document.index.getElementById{'email_input'};
+ var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+ if (!reg.test(email.value)){
+ alert("sdvdfvdfvd"); 
+}
+ else
+ {
+ alert("sdcdscsdc");
+ }
+} 
+
+</script>
