3c5543bfc796c91488f9dfe1f08e6f99ebdc241d
Marcus Freemon <dfreemon@gmail.com>
2015-02-07 16:18:44 -0800

    updates to js file to include namespacing

--- js/pumpjack.js
+++ js/pumpjack.js
@@ -1,54 +1,73 @@
 
-$(document).ready(function(){
+
+
+var pumpjack ={};	
 
 // simple mousenter functionality//
-$(".container1 div").on("mouseenter", function(){
-	$(this).css("color", "#09d017");})
+
+pumpjack.mousenter = function(){
+	$(".container1 div").on("mouseenter", function(){
+	$(this).css("color", "#09d017");
+	})
 	.on("mouseleave", function(){
 		$(".container1 div").css("color", "#f5f5f5");
 	});
+};	
 
 
 
 // toogling between hidden content using arrow animation//
-$(".interestpara").click (function(){
-	$('#careerpara').slideToggle();
-	$('#arrow').toggleClass('active');
+pumpjack.parahide = function(){
+	$(".interestpara").click (function(){
+		$('#careerpara').slideToggle();
+		$('#arrow').toggleClass('active');
+	});
+};
 
-});
 
-
-// smooth scrolling to anchor location vs instantaneous jumping to section/
-$('a').click(function(){
+// smooth scrolling to anchor location vs instantaneous jumping to section//
+pumpjack.smoothscroll = function(){
+	$('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top -60
     },  800);
     return false;
-});
-
-
-var navscroll = $(".navbar").offset().top + $(".navbar").height();
-$(window).on('scroll', function(){
-	stop = Math.round($(window).scrollTop());
-	if (stop >= navscroll){
-		$(".navbar").addClass("navfloat");
-         }
-		else
-		{
-		$(".navbar").removeClass("navfloat");}
-});
-
-var $window = $(window);
-$('section[data-type = "background"]').each(function(){
-	var $bgobj=$(this);
-	$(window).scroll(function(){
-	var yPos =-($window.scrollTop()/$bgobj.data('speed'));
-	var coords = '50%' + yPos + 'px';
-	$bgobj.css({backgroundPosition:coords})
-
 	});
+};
+
+
+pumpjack.navchange = function(){
+	var navscroll = $(".navbar").offset().top + $(".navbar").height();
+	$(window).on('scroll', function(){
+		stop = Math.round($(window).scrollTop());
+		if (stop >= navscroll){
+			$(".navbar").addClass("navfloat");
+      	   }
+			else
+			{
+			$(".navbar").removeClass("navfloat");
+		}
+	});
+};	
+
+pumpjack.parallax = function(){
+	var $window = $(window);
+	$('section[data-type = "background"]').each(function(){
+		var $bgobj=$(this);
+		$(window).scroll(function(){
+			var yPos =-($window.scrollTop()/$bgobj.data('speed'));
+			var coords = '50%' + yPos + 'px';
+			$bgobj.css({backgroundPosition:coords});
+		});
+	});
+};
+
+$(document).ready(function(){
+	pumpjack.mousenter();
+	pumpjack.parahide();
+	pumpjack.smoothscroll();
+	pumpjack.navchange();
+	pumpjack.parallax();
+
 });
 
-
-
-});
