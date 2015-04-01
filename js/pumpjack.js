aefcf5769778018e713ef8b09976f924fed02ec1
Marcus Freemon <dfreemon@gmail.com>
2015-03-31 22:42:52 -0700

    structure changes

--- js/pumpjack.js
+++ js/pumpjack.js
@@ -50,17 +50,17 @@ pumpjack.navchange = function(){
 	});
 };	
 
-pumpjack.parallax = function(){
-	var $window = $(window);
-	$('section[data-type = "background"]').each(function(){
-		var $bgobj=$(this);
-		$(window).scroll(function(){
-			var yPos =-($window.scrollTop()/$bgobj.data('speed'));
-			var coords = '50%' + yPos + 'px';
-			$bgobj.css({backgroundPosition:coords});
-		});
-	});
-};
+// pumpjack.parallax = function(){
+// 	var $window = $(window);
+// 	$('section[data-type = "background"]').each(function(){
+// 		var $bgobj=$(this);
+// 		$(window).scroll(function(){
+// 			var yPos =-($window.scrollTop()/$bgobj.data('speed'));
+// 			var coords = '50%' + yPos + 'px';
+// 			$bgobj.css({backgroundPosition:coords});
+// 		});
+// 	});
+// };
 
 $(document).ready(function(){
 	pumpjack.mousenter();
